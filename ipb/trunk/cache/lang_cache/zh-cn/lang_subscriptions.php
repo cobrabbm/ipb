927bd3acbf0220da7c9a9a53ca4921ccbf907e69
skylook@126.com <skylook@126.com@d4cd8844-c944-0410-9ffc-abee0f7048fe>
2008-06-22 15:13:47 +0000

    易维论坛 v2.3.5 Final

--- ipb/trunk/cache/lang_cache/zh-cn/lang_subscriptions.php
+++ ipb/trunk/cache/lang_cache/zh-cn/lang_subscriptions.php
@@ -1,81 +1,116 @@
 <?php
 
-$lang = array(
+/*
++--------------------------------------------------------------------------
+|   Invision Power Board v2.3.5 Chinese Language Package
+|   =======================================================================
+|   作者: Skylook
+|   =======================================================================
+|   网站: http://www.ipbchina.com
+|   发布: 2008-06-22
+|   语言包版权归 IPBChina.COM 所有
++--------------------------------------------------------------------------
+*/
 
-'paywith_nochex'  => "现在就用 NOCHEX 网关进行支付",
-'paywith_paypal'  => "用 PayPal 网关进行进行支付 - 感受快捷、自由、安全的服务!",
-'paywith_protx'  => "用 Protx 网关进行进行支付",
-'paywith_gen'  => "购买",
-'no_expire'  => "请选择其他的订阅包裹",
-'msg_reg_title'  => "感谢您的注册!",
+$lang = array (
+
+'paywith_nochex' => "现在就用 NOCHEX 网关进行支付",
+'paywith_paypal' => "用 PayPal 网关进行进行支付 - 感受快捷、自由、安全的服务!",
+'paywith_protx' => "用 Protx 网关进行进行支付",
+'paywith_gen' => "购买",
+
+'no_expire' => "请选择其他的订阅包裹",
+'msg_reg_title' => "感谢您的注册!",
 'msg_reg_text'  => "在注册期间,您选择购买一个订阅包裹.<br />请重新查看以下信息以确认您的选择.",
 'msg_gen_title'  => "订阅包裹",
-'msg_gen_text'  => "谢谢访问我们的包裹订阅面板.<br />请选择一个以下包裹以选择可用的付费方式.",
-'msg_force_title'  => "请购买一个订阅包裹",
+'msg_gen_text'   => "谢谢访问我们的包裹订阅面板.<br />请选择一个以下包裹以选择可用的付费方式.",
+
+'msg_force_title' => "请购买一个订阅包裹",
 'msg_force_text'  => "我们需要您在进入论坛之前购买一个订阅包裹.",
-'cc_currency_in'  => "所有的价格用",
-'cc_change'  => "更换",
-'man_tick_title'  => "请打印这张单子并附带在款项中",
-'man_mem'  => "会员名",
-'man_mem_id'  => "会员 ID",
-'man_email'  => "会员邮件",
-'man_trans_id'  => "转换 ID",
-'man_sub'  => "订阅标题",
-'man_pay'  => "款项附带的",
-'man_title'  => "汇款单",
-'gw_upgrade'  => "升级",
+
+'cc_currency_in' => "所有的价格用",
+'cc_change'      => "更换",
+
+'man_tick_title' => "请打印这张单子并附带在款项中",
+'man_mem'        => "会员名",
+'man_mem_id'     => "会员 ID",
+'man_email'      => "会员邮件",
+'man_trans_id'   => "转换 ID",
+'man_sub'        => "订阅标题",
+'man_pay'        => "款项附带的",
+'man_title'      => "汇款单",
+
+
+
+'gw_upgrade' => "升级",
 'gw_protx'  => "点击以下按钮通过 Protx 网站完成订购项目",
+
 'gw_paypal'  => "点击以下按钮通过 PayPal 网站完成订购项目. 所有的价格在 PayPal 的网站中都要以美元为单位",
 'gw_nochex'  => "点击以下按钮通过 NOCHEX 网站完成订购项目. 所有的价格在 NOCHEX 网站都要以英镑为单位",
 'gw_safshop'  => "点击以下按钮通过 Safshop 的网站完成订购项目. 所有的价格在 Safshop 网站都要以美元为单位",
-'gw_authorize'  => "点击以下按钮通过 我们的在线销售页面 完成订购项目.",
-'post_manual'  => "您选择通过邮局汇款付费. 当您点击以下按钮时, 您的订购请求将被标记为 '处理中' 同时将会弹出一个包含了一张票据的新窗口, 您可以将它打印出来并且夹带在汇款中. 但在我们收到您的汇款并进行处理之前您暂时还不能使用付费订阅项目.",
-'post_manual_more'  => "请按照上面票据中的数目将指定款项汇到:<br /><b>%s<br />%s<br />%s<br />%s<br />%s</b><br /><br />如果您需要知道汇率, 请访问 <a href='http://www.xe.com'>这个网站</a>.",
-'gw_manual'  => "点击这里继续...",
-'sc_complete'  => "完成购买中...",
-'sc_upgrade_title'  => "续费确认",
-'sc_normal_title'  => "付费确认",
-'sc_upgrade_explain'  => "在继续前请重新查看订购的详细资料.",
-'sc_upgrade_string'  => "您选择了续费从 <b>%s</b> 到 <b>%s</b>, 该服务到期时间 <b>%s</b>.<br />本次续费的价格是 <b>%s</b> 并且您的服务将在 <b>%s</b> 到期.<br /><br />请选择一种付费方式然后继续以下步骤.",
-'sc_normal_string'  => "您选择为订阅项目 <b>%s</b> 续费.<br /> 价格是 <b>%s</b>.",
-'sc_available_methods'  => "可用的付费方式",
-'sc_method'  => "付费方式",
-'s_page_title'  => "订阅管理面板",
-'s_current_subs'  => "您当前订阅的项目",
-'s_current_explain'  => "您所订阅包裹的详细资料.",
-'s_dead_subs'  => "已经到期的订阅项目",
-'s_dead_explain'  => "所有通过的或者取消的订阅项目.",
-'s_continue_button'  => "继续选择一个付费方式...",
-'s_continue_button2'  => "继续...",
-'s_upgrade_subs'  => "升级到新的订阅",
-'s_upgrade_explain'  => "您可以升级到任何以下订阅项目. 您只需要支付价差即可. 订阅成功后将用您新的订阅项目覆盖以前的直到订阅项目到期.",
-'s_started'  => "开始日期",
-'s_end'  => "结束日期",
-'s_paid'  => "付费",
-'s_status'  => "状态",
-'s_transid'  => "ID",
-'s_balance_cost'  => "欠款",
-'s_current_bottom'  => "如果您有任何关于当前或最近订阅的问题, 请与我们的管理用户组联络.",
-'s_available_subs'  => "您可用的订阅项目",
-'s_detail'  => "详细资料",
-'s_duration'  => "持续时间",
-'s_cost'  => "价格",
-'s_explain'  => "订阅: 简单地点击订阅项目旁边的按钮并且选择以下付费方式. 点击继续按钮将带您到确认页面, 您可以看到更多关于您所选付费方式的资料.",
-'pay_paid'  => "已付费",
-'pay_failed'  => "已失效",
-'pay_pending'  => "验证中",
-'pay_expired'  => "已过期",
-'pay_dead'  => "已取消",
-'time_y'  => "年",
-'time_m'  => "月",
-'time_d'  => "日",
-'time_w'  => "星期",
-'timep_y'  => "年",
-'timep_m'  => "月",
-'timep_d'  => "日",
-'timep_w'  => "星期",
-'gw_authorizenet'  => "Click the button below to complete this order via our secure online payment page.",
+'gw_authorize' => "点击以下按钮通过 我们的在线销售页面 完成订购项目.",
 
+'post_manual' => "您选择通过邮局汇款付费. 当您点击以下按钮时, 您的订购请求将被标记为 '处理中' 同时将会弹出一个包含了一张票据的新窗口, 您可以将它打印出来并且夹带在汇款中. 但在我们收到您的汇款并进行处理之前您暂时还不能使用付费订阅项目.",
+'post_manual_more' => "请按照上面票据中的数目将指定款项汇到:<br /><b>%s<br />%s<br />%s<br />%s<br />%s</b><br /><br />如果您需要知道汇率, 请访问 <a href='http://www.xe.com'>这个网站</a>.",
+
+'gw_manual'  => "点击这里继续...",
+
+'sc_complete' => "完成购买中...",
+
+'sc_upgrade_title' => "续费确认",
+'sc_normal_title' => "付费确认",
+'sc_upgrade_explain' => "在继续前请重新查看订购的详细资料.",
+'sc_upgrade_string' => "您选择了续费从 <b>%s</b> 到 <b>%s</b>, 该服务到期时间 <b>%s</b>.<br />本次续费的价格是 <b>%s</b> 并且您的服务将在 <b>%s</b> 到期.<br /><br />请选择一种付费方式然后继续以下步骤.",
+
+'sc_normal_string'  => "您选择为订阅项目 <b>%s</b> 续费.<br /> 价格是 <b>%s</b>.",
+
+'sc_available_methods' => "可用的付费方式",
+'sc_method' => "付费方式",
+
+'s_page_title' => "订阅管理面板",
+'s_current_subs'    => "您当前订阅的项目",
+'s_current_explain' => "您所订阅包裹的详细资料.",
+'s_dead_subs'    => "已经到期的订阅项目",
+'s_dead_explain' => "所有通过的或者取消的订阅项目.",
+
+'s_continue_button' => "继续选择一个付费方式...",
+'s_continue_button2' => "继续...",
+
+'s_upgrade_subs'    => "升级到新的订阅",
+'s_upgrade_explain' => "您可以升级到任何以下订阅项目. 您只需要支付价差即可. 订阅成功后将用您新的订阅项目覆盖以前的直到订阅项目到期.",
+
+'s_started'      => "开始日期",
+'s_end'          => "结束日期",
+'s_paid'         => "付费",
+'s_status'		 => "状态",
+'s_transid'      => "ID",
+'s_balance_cost' => "欠款",
+
+'s_current_bottom' => "如果您有任何关于当前或最近订阅的问题, 请与我们的管理用户组联络.",
+
+'s_available_subs' => "您可用的订阅项目",
+'s_detail'         => "详细资料",
+'s_duration'	   => "持续时间",
+'s_cost'		   => "价格",
+'s_explain'		   => "订阅: 简单地点击订阅项目旁边的按钮并且选择以下付费方式. 点击继续按钮将带您到确认页面, 您可以看到更多关于您所选付费方式的资料.",
+
+'pay_paid'    => "已付费",
+'pay_failed'  => "已失效",
+'pay_pending' => "验证中",
+'pay_expired' => "已过期",
+'pay_dead'    => "已取消",
+
+'time_y'      => "年",
+'time_m'      => "月",
+'time_d'      => "日",
+'time_w'      => "星期",
+
+'timep_y'     => "年",
+'timep_m'     => "月",
+'timep_d'     => "日",
+'timep_w'     => "星期",
+
+'gw_authorizenet'	=> "点击下面这个按钮通过我们的 在线支付系统 完成您的订单.",
 );
 
 ?>
